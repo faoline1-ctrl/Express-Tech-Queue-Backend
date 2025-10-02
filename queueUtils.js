b678d28dbf0b9cf1a19dbf366b2313265ca519af
Justin <faoline1@gmail.com>
2025-10-01 19:36:51 -0600

    Reinitialize commit

---
+++ queueUtils.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const DATA_PATH = path.join(__dirname, 'technicianQueue.json');
+
+function readQueue() {
+  try {
+    return JSON.parse(fs.readFileSync(DATA_PATH, 'utf8'));
+  } catch (err) {
+    return [];
+  }
+}
+
+function writeQueue(queue) {
+  fs.writeFileSync(DATA_PATH, JSON.stringify(queue, null, 2));
+}
+
+module.exports = { readQueue, writeQueue };
